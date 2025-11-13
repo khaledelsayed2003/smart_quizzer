b92c821ce485a72dc92bdf431d4b0e430ae60ba6
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-11-13 23:28:26 +0300

    feat: initialize full quiz functionality with API data loader, models, and quiz controller

---
+++ src/data/data.py
@@ -0,0 +1,16 @@
+import requests
+
+num_of_questions = 50
+difficulty = "hard"
+type_of_questions = "boolean"
+
+parameters = {
+    "amount":num_of_questions,
+    "difficulty": difficulty,
+    "type": type_of_questions
+}
+
+response = requests.get(url="https://opentdb.com/api.php", params= parameters)
+response.raise_for_status()
+data = response.json()
+question_data = data['results']
